ec4852e220c5ce9e4bf724b67ba41d64fa0d202a
ivbmtest <ivbmittest@gmail.com>
2023-12-25 15:05:32 +0530

    create and del added

---
+++ git/IVBMCS/admin_panel/models.py
@@ -0,0 +1,176 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+# Create your models here.
+
+
+class txmst(models.Model):
+    txid = models.AutoField(primary_key=True, db_column='txid',verbose_name='Tax Id')
+    txname=models.CharField(max_length=255,verbose_name='Tax Name',unique=True)
+    txdescription = models.CharField(max_length=255,verbose_name='Tax Description')
+    txisgst = models.BooleanField(default=True,verbose_name='GST')
+    txstatus = models.BooleanField(default=True,verbose_name='Status')
+    usrid = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, editable=False,verbose_name="Created By")
+    dtupdatd = models.DateTimeField(auto_now_add=True,verbose_name='Created On')
+       
+    class Meta:
+        verbose_name = "Tax Master"
+        verbose_name_plural = "Tax Master"
+        db_table = 'txmst'
+        
+    def __str__(self):
+        return self.txname
+    
+    
+class txdet(models.Model):
+    tdid = models.AutoField(primary_key=True, db_column='tdid',verbose_name='Tax Detais Id')
+    tdtxid = models.ForeignKey(txmst,on_delete=models.CASCADE,verbose_name='Tax Id')
+    tdname = models.CharField(max_length=255,verbose_name='Tax Details Name')
+    tddescription = models.CharField(max_length=255,verbose_name='Tax Details Description')
+    tdcgst = models.DecimalField(max_digits=5, decimal_places=2,verbose_name='CGST Rate')
+    tdsgst = models.DecimalField(max_digits=5, decimal_places=2,verbose_name='SGST Rate')
+    tdigst = models.DecimalField(max_digits=5, decimal_places=2,verbose_name='IGST Rate')
+    tdvat = models.DecimalField(max_digits=5, decimal_places=2,verbose_name='VAT Rate')
+    tdcess = models.DecimalField(max_digits=5, decimal_places=2,verbose_name='CESS Rate')
+    tdstatus = models.BooleanField(default=True, verbose_name='Status')
+    usrid = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, editable=False,verbose_name="Created By")
+    dtupdatd = models.DateTimeField(auto_now_add=True,verbose_name='Created On')
+    
+    
+    class Meta:
+        verbose_name = "Tax Details"
+        verbose_name_plural = "Tax Details"
+        db_table = 'txdet'
+        
+    def __str__(self):
+        return self.tdname
+        
+        
+class formt(models.Model):
+    frid = models.AutoField(primary_key=True, db_column='tdid',verbose_name='Id')
+    frname = models.CharField(max_length=255,verbose_name='Name',unique=True)
+    frdescription = models.CharField(max_length=255,verbose_name='Description')
+    frfltr = models.CharField(max_length=255,verbose_name='Format Filter')
+    frstatus = models.BooleanField(default=True, verbose_name='Status')
+    usrid = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, editable=False,verbose_name="Created By")
+    dtupdatd = models.DateTimeField(auto_now_add=True,verbose_name='Created On')
+    
+    class Meta:
+        verbose_name = "File Format Details"
+        verbose_name_plural = "File Format Details"
+        db_table = 'formt'
+        
+    def __str__(self):
+        return self.frname
+        
+    
+class crnc(models.Model):
+    crid = models.AutoField(primary_key=True, db_column='tdid',verbose_name='Id')
+    crname = models.CharField(max_length=100,verbose_name='Name')
+    crsymbol = models.CharField(max_length=10,verbose_name='Symbol')
+    crdescription = models.TextField(verbose_name='Description')
+    crstatus = models.BooleanField(default=True,verbose_name='Status')
+    usrid = models.ForeignKey(User,  on_delete=models.SET_NULL, null=True, editable=False,verbose_name="Created By")
+    dtupdatd = models.DateTimeField(auto_now_add=True,verbose_name='Created On')
+    
+    class Meta:
+        verbose_name = "Currency"
+        verbose_name_plural = "Currency"
+        db_table = 'crnc'
+    
+    def __str__(self):
+        return self.crname
+    
+
+class cntry(models.Model):
+    cnid = models.AutoField(primary_key=True, db_column='tdid',verbose_name='Id')
+    cnname = models.CharField(max_length=100,verbose_name='Name')
+    cndescription = models.TextField(verbose_name='Description')
+    cncrid =models.ForeignKey(crnc,  on_delete=models.CASCADE,verbose_name='Currency')
+    cntxid = models.ForeignKey(txmst, on_delete=models.CASCADE,verbose_name='Tax')
+    # Tax = models.TextField()
+    cnstatus = models.BooleanField(default=True,verbose_name='Status')
+    usrid = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, editable=False,verbose_name="Created By")
+    dtupdatd = models.DateTimeField(auto_now_add=True,verbose_name='Created On')
+    
+    class Meta:
+        verbose_name = "Country"
+        verbose_name_plural = "Country"
+        db_table = 'cntry'
+    
+    def __str__(self):
+        return self.cnname
+    
+
+class ctgry(models.Model):
+    ctid = models.AutoField(primary_key=True, db_column='tdid',verbose_name='Id')
+    ctname = models.CharField(max_length=50,verbose_name='Name')
+    ctcode = models.CharField(max_length=50,verbose_name='Code')
+    ctdescription = models.TextField(verbose_name='Description')
+    ctimg = models.ImageField(upload_to='images/',verbose_name='Image')
+    ctstatus = models.BooleanField(default=True,verbose_name='Status')
+    usrid = models.ForeignKey(User,  on_delete=models.SET_NULL, null=True, editable=False,verbose_name="Created By")
+    dtupdatd = models.DateTimeField(auto_now_add=True,verbose_name='Created On')
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Category"
+        db_table = 'ctgry'
+        
+    def __str__(self):
+        return self.ctname
+    
+class srvc(models.Model):
+    svid = models.AutoField(primary_key=True, db_column='tdid',verbose_name='Tax Detais Id')
+    svcode = models.CharField(max_length=255,unique=True,verbose_name='Code')
+    svname = models.CharField(max_length=255,unique=True,verbose_name='Name')
+    svdescription = models.CharField(max_length=255,verbose_name='Description')
+    svcategory = models.ForeignKey(ctgry, on_delete=models.CASCADE,verbose_name='Category')
+    svcountry = models.ForeignKey(cntry, on_delete=models.CASCADE,verbose_name='Country')
+    svrate = models.DecimalField(max_digits=10, decimal_places=2,verbose_name='Rate')  #Government Rate
+    svratesplit = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Rate Split Up") #Rate Splitup of value in svrate column
+    svsrvchg = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Service chg") #IVBMCS Service chg
+    discrt = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Discount Rate")
+    discamt = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Discount Amt")
+    disctxt = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Discount Text")
+    svagamt = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Agent Amt")
+    svagrt = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Agent Rate")
+    svtxid = models.ForeignKey(txmst, on_delete=models.CASCADE,verbose_name="Tax")
+    svtxrt = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Tax Rate")
+    svtxamt = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Tax Amt")
+    svnetamt = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Net Amt")
+    svstatus = models.BooleanField(default=True,verbose_name='Status')
+    usrid = models.ForeignKey(User,  on_delete=models.SET_NULL, null=True, editable=False,verbose_name="Created By")
+    dtupdatd = models.DateTimeField(auto_now_add=True,verbose_name='Created On')
+    
+    class Meta:
+        verbose_name = "Services"
+        verbose_name_plural = "Services"
+        db_table = 'srvc'
+    
+    def __str__(self):
+        return self.svname
+    
+# class DocumentsRequired(models.Model):
+#     tdid = models.AutoField(primary_key=True, db_column='tdid',verbose_name='Tax Detais Id')
+#     Service = models.ForeignKey(srvc, on_delete=models.CASCADE)
+#     DocumentName = models.CharField(max_length=50,verbose_name='Document Name')
+#     Description = models.TextField()
+#     # Format = models.CharField(max_length=50,choices=[('option1', 'PDF only'), ('option2', 'PDF/JPEG/PNG')])
+#     Format = models.ForeignKey(formt, on_delete=models.CASCADE)
+#     MaxFileSize = models.IntegerField(verbose_name= 'Max File Size')
+#     Status = models.BooleanField(default=True)
+#     CreatedBy = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, editable=False,verbose_name="Created By")
+#     CreatedOn = models.DateTimeField(auto_now_add=True,verbose_name='Created On')
+    
+    
+    
+
+    
+
+ 
+
+    
+
+
---
+++ git/IVBMCS/admin_panel/views.py
@@ -0,0 +1,152 @@
+from django.shortcuts import render, HttpResponse, redirect,get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .models import *
+from .form import *
+from django.http import JsonResponse
+
+def Login(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user_det = authenticate(request, username=username, password=password)
+        
+        if user_det is not None:
+            user = get_object_or_404(User, username=username)
+            if user.is_superuser:
+                login(request, user_det)
+                # Explicitly set the session to save the changes
+                request.session.save()
+                # Redirect to the Django admin page after successful login
+                return redirect('admin:index')
+            else:
+                return render(request, 'main_layout.html',{'user':user})
+            
+        else:
+            # Handle invalid login credentials
+            return render(request, 'admin/login.html', {'error_message': 'Invalid credentials'})
+
+    return render(request, 'admin/login.html')
+
+# from django.shortcuts import render
+
+# Create your views here.
+
+
+# def index(request):
+#     #  return redirect('/admin/')
+#     return render(request,'admin.html')
+
+def currency(request):
+    print("test ajax")
+    if request.method == 'POST':
+        form = crncForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            #my_field_data = form.cleaned_data['crname']
+            form.save()
+
+            return JsonResponse({'success': True, 'message': 'Form submitted successfully'})
+        else:
+            return JsonResponse({'success': False, 'message': 'All Fields must fill !'})
+    else:
+        frm = crncForm()   
+        model_meta = crnc._meta
+        field_names = [field.verbose_name for field in model_meta.fields]
+        print("else")
+        currency_info=crnc.objects.all()
+        return render(request,'currency.html',{'form': frm,'currency_info':currency_info,'field_names': field_names})
+
+
+#del currency
+def del_currency(request):
+    if request.method == 'POST':
+        id = request.POST['crid']
+        print(id)
+        currency = crnc.objects.get(crid=id)
+        currency.delete()
+        return JsonResponse({'success': True, 'message': 'delete ok !'})
+
+
+def category(request):
+    if request.method == 'POST':
+        frm = CategoryForm(request.POST)
+        
+        if frm.is_valid:
+            frm.save()
+            return redirect('success_url')
+    else:
+        frm = CategoryForm()
+        
+    model_meta = ctgry._meta
+    field_names = [field.verbose_name for field in model_meta.fields]
+        
+    currency_info=ctgry.objects.all()
+    return render(request,'category.html',{'form': frm,'currency_info':currency_info,'field_names': field_names})
+
+
+
+def country(request):
+    if request.method == 'POST':
+        frm = CountryForm(request.POST)
+        
+        if frm.is_valid:
+            frm.save()
+            return redirect('success_url')
+    else:
+        frm = CountryForm()
+        
+    model_meta = cntry._meta
+    field_names = [field.verbose_name for field in model_meta.fields]
+        
+    currency_info=cntry.objects.all()
+    return render(request,'country.html',{'form': frm,'currency_info':currency_info,'field_names': field_names})
+
+def document(request):
+    return render(request,'document.html')
+
+
+def services(request):
+    if request.method == 'POST':
+        frm = ServiceForm(request.POST)
+        
+        if frm.is_valid:
+            frm.save()
+            return redirect('success_url')
+    else:
+        frm = ServiceForm()
+        
+    model_meta = srvc._meta
+    field_names = [field.verbose_name for field in model_meta.fields]
+        
+    currency_info=srvc.objects.all()
+    return render(request,'services.html',{'form': frm,'currency_info':currency_info,'field_names': field_names}) 
+
+def dashboard(request):
+    return render(request,'main_layout.html')
+
+def logout(request):
+    return render(request,'admin/login.html')
+
+
+# def crncform(request):
+#     if request.method == 'POST':
+#         frm = crncForm(request.POST)
+        
+#         if frm.is_valid:
+#             frm.save()
+#             return redirect('success_url')
+#     else:
+#         frm = crncForm()
+        
+#     return render(request, 'currency.html', {'form': frm})
+
+
+
+# def db_details(request):
+    
+    
+    
