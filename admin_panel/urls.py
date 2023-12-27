14f91e09045c176ecc8ef4eeb41e6ab31ad567f7
ivbmtest <ivbmittest@gmail.com>
2023-12-27 09:42:01 +0530

    add, search,delete using jquery

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -14,11 +14,14 @@ urlpatterns = [
     path('dashboard/',views.dashboard,name='dashboard'),
     # path('add_user/',views.crncform,name='add_user'),
     path('logout/',views.logout,name='logout'),
-
     #add curre
     #path('add_curr/',views.add_currency,name='add_curr'),
     # del_currency
     path('del_currency/',views.del_currency,name='del_currency'),
+    path('select_del/',views.select_del,name='select_del'),
+
+
+    path('curr_ser/',views.curr_ser,name='ser'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, HttpResponse, redirect,get_object_or_404
 from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
+#from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import *
 from .form import *
+from django.db.models.query_utils import Q
 from django.http import JsonResponse
+from django.core.paginator import Paginator
+from django.forms.models import model_to_dict
+
 
 def Login(request):
     
@@ -41,24 +45,27 @@ def Login(request):
 #     return render(request,'admin.html')
 
 def currency(request):
-    print("test ajax")
     if request.method == 'POST':
         form = crncForm(request.POST)
         if form.is_valid():
             # Process the form data
             #my_field_data = form.cleaned_data['crname']
             form.save()
-
-            return JsonResponse({'success': True, 'message': 'Form submitted successfully'})
-        else:
-            return JsonResponse({'success': False, 'message': 'All Fields must fill !'})
+            return redirect('currency')
+            #return JsonResponse({'success': True, 'message': 'Form submitted successfully'})
+        #else:
+            #return JsonResponse({'success': False, 'message': 'All Fields must fill !'})
     else:
         frm = crncForm()   
         model_meta = crnc._meta
         field_names = [field.verbose_name for field in model_meta.fields]
-        print("else")
-        currency_info=crnc.objects.all()
-        return render(request,'currency.html',{'form': frm,'currency_info':currency_info,'field_names': field_names})
+        y=crnc.objects.all()
+        page=Paginator(y,5)
+        page_list=request.GET.get('page')
+        page=page.get_page(page_list)
+        print(page)
+        #currency_info=crnc.objects.all()
+        return render(request,'currency.html',{'form': frm,'currency_info':page,'field_names': field_names})
 
 
 #del currency
@@ -70,20 +77,75 @@ def del_currency(request):
         currency.delete()
         return JsonResponse({'success': True, 'message': 'delete ok !'})
 
+#currency search
+def curr_ser2(request):
+    if 'term' in request.GET:
+        h = request.GET['term']
+        #print(h)
+        fields_to_search = ['crname', 'crsymbol','crdescription']
+        # Build a Q object for each field
+        queries = [Q(**{f'{field}__startswith': h}) for field in fields_to_search]
+        # Combine the queries with OR condition
+        search_query = queries.pop()
+        for query in queries:
+            search_query |= query
+        # Perform the search using the Q object
+        results = crnc.objects.filter(search_query)
+
+        context = {
+            'results': results,
+            'query': query,
+        }
+        #print(results)
+        #l = crnc.objects.filter(Q(crname__startswith=h) | Q(crsymbol__startswith=h))
+        dot = []
+        #print(l)
+        s = list(results.values())
+        #print(s)
+        pac = []
+        for j in s:
+            pac.append(j.values())
+        #print(pac)
+        #pac=type(pac)
+        print("list",pac)
+        for i in results:
+            #print(type(i))
+            dot.append(i.crname)
+        return JsonResponse(dot,safe = False)
+    return redirect('/')
+
+def curr_ser(request):
+    if request.method == 'POST':
+        d = request.POST['ser_name']
+        fields_to_search = ['crname', 'crsymbol','crdescription']
+        queries = [Q(**{f'{field}__startswith': d}) for field in fields_to_search]
+        # Combine the queries with OR condition
+        search_query = queries.pop()
+        for query in queries:
+            search_query |= query
+        # Perform the search using the Q object
+        print("ser",search_query)
+        results = crnc.objects.filter(search_query)
+        print(results)
+        frm = crncForm()   
+        model_meta = crnc._meta
+        field_names = [field.verbose_name for field in model_meta.fields]
+        return render(request,'currency.html',{'form': frm,'currency_info':results,'field_names': field_names})
+
+
+
+
 
 def category(request):
     if request.method == 'POST':
-        frm = CategoryForm(request.POST)
-        
+        frm = CategoryForm(request.POST)  
         if frm.is_valid:
             frm.save()
             return redirect('success_url')
     else:
-        frm = CategoryForm()
-        
+        frm = CategoryForm() 
     model_meta = ctgry._meta
-    field_names = [field.verbose_name for field in model_meta.fields]
-        
+    field_names = [field.verbose_name for field in model_meta.fields]    
     currency_info=ctgry.objects.all()
     return render(request,'category.html',{'form': frm,'currency_info':currency_info,'field_names': field_names})
 
@@ -97,13 +159,20 @@ def country(request):
             frm.save()
             return redirect('success_url')
     else:
-        frm = CountryForm()
-        
+        frm = CountryForm() 
     model_meta = cntry._meta
     field_names = [field.verbose_name for field in model_meta.fields]
         
     currency_info=cntry.objects.all()
     return render(request,'country.html',{'form': frm,'currency_info':currency_info,'field_names': field_names})
+
+
+def select_del(request):
+    a=request.GET['select']
+    print(a)
+   
+    return redirect('currency')
+
 
 def document(request):
     return render(request,'document.html')
@@ -112,13 +181,11 @@ def document(request):
 def services(request):
     if request.method == 'POST':
         frm = ServiceForm(request.POST)
-        
         if frm.is_valid:
             frm.save()
             return redirect('success_url')
     else:
         frm = ServiceForm()
-        
     model_meta = srvc._meta
     field_names = [field.verbose_name for field in model_meta.fields]
         
