19464076f3358b4ca1d409135963bb008af8f99c
ivbmtest <ivbmittest@gmail.com>
2024-03-12 09:10:17 +0530

    tabs in user dash,staff task divide own categoery

--- ivbmcs/user_portal/context_processors.py
+++ ivbmcs/user_portal/context_processors.py
@@ -4,6 +4,6 @@ from admin_panel.models import *
 def select_service(request):
     # category=ctgry.objects.all()
     category = ctgry.objects.prefetch_related('srvc_set').all()
-    print(">>>>>>>>>>>>>>>>",category)
+    #print(">>>>>>>>>>>>>>>>",category)
     return {'category':category}
 
